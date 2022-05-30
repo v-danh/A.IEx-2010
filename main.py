b80bba7455e7eea8d7c11a7af3fcd237c7501772
v-danh <vdanhvt2000@gmail.com>
2022-05-30 13:51:51 +0700

    made changes

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ import lab0
 # __name__ == '__main__'
 if __name__ == '__main__':
     print('CUBE = {}'.format(lab0.cube(2)))
+    print('CUBE = {}'.format(lab0.cube(4)))
+    
+    print('FACTORIAL = {}'.format(lab0.factorial(2)))
     print('FACTORIAL = {}'.format(lab0.factorial(4)))
